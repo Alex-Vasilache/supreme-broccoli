21c637b58a19a70f422cf31f64c065d5d14c04ea
Alex Vasilache <alex.vasilache9@gmail.com>
2020-01-26 10:44:47 +0100

    Created BluetoothManager class to move eSenseManager functionality

---
+++ lib/bluetoothManager.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:newprojectx/box-game.dart';
+
+class BluetoothManager {
+
+  final BoxGame game; 
+
+  BluetoothManager(this.game) {
+    
+  }
+
+  void render(Canvas c) {}
+
+  void update(double t) {}
+
+}
